bcd27e2f0c6b34afe144c928c1c04bfeb1832f05
Aung27 <aungsing3335@gmail.com>
2025-06-23 00:30:55 +0600

    complete the code

--- It's Time To Duel.cpp
+++ It's Time To Duel.cpp
@@ -3,7 +3,39 @@ using namespace std;
 
 void solve()
 {
-    
+    int n;
+    cin >> n;
+    int a[n];
+    int cn1 = 0, cn0 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if (a[i] == 1)
+        {
+            cn1++;
+        }
+        else
+        {
+            cn0++;
+        }
+    }
+    if (cn0 == n || cn1 == n)
+    {
+        cout << "YES" << endl;
+        return;
+    }
+    else
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (a[i] == 0 && a[i+1] == 0)
+            {
+                cout << "YES" << endl;
+                return;
+            }
+        }
+    }
+    cout << "NO" << endl;
 }
 
 int main()
