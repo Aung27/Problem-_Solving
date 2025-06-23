b75afb3850073fd14f4fdc357bdafed427731cb2
Aung27 <aungsing3335@gmail.com>
2025-06-23 23:22:03 +0600

    Complete the code

---
+++ Shrink.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+// okay
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 2; i <= n; i++)
+    {
+        cout << i << ' ';
+    }
+    cout << 1 << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+
+    return 0;
+}
